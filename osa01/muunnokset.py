c2ba9052d703c78b570084bcc17da34cb7e3db00
Teemu H <44917122+swd1tn002@users.noreply.github.com>
2023-09-05 17:12:46 +0000

    Osa 1 esimerkit

---
+++ osa01/muunnokset.py
@@ -0,0 +1,5 @@
+hinta_str = input("Anna lounaan hinta: ")
+hinta_float = float(hinta_str)
+
+print(hinta_str * 5)    # toistaa merkkijoona 5 kertaa
+print(hinta_float * 5)  # kertoo annetun luvun luvulla 5
---
+++ osa01/printti.py
@@ -0,0 +1,5 @@
+nimi = "Arto"
+ika = 39
+kaupunki = "Espoo"
+
+print(f"Hei {nimi}, olet {ika}-vuotias. Asuinpaikkasi on {kaupunki}.")
